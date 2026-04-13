68db5653eb9a3b303490d2b940e465c989850973
Mamun123123123 <almalm90459@gmail.com>
2026-04-13 19:42:13 +0600

    comment and data type

--- mamun.py
+++ mamun.py
@@ -1,1 +1,16 @@
 print("ki obsthaaaaaaaa")
+#hello single line comment
+""""hedfdhfd multiline"""
+
+name = "Mamun"
+age = 100
+print(type(age))
+print(type(name))
+
+#Numbers
+a=1
+b=1.5
+c=4j
+print(type(a))
+print(type(b))
+print(type(c))
