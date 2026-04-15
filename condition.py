a90ba007f227fe7b6134bc58687b69ea79ffb042
Mamun123123123 <almalm90459@gmail.com>
2026-04-15 18:55:44 +0600

    condition

---
+++ condition.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter your first number "))
+num2 = int(input("Enter your second number "))
+
+if num1>num2:
+    print(f"{num1} is greater")
+else:
+    print(f"{num2}")    
+
+
+
