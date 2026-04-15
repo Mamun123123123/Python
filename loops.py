af4fd32c6a016c902aeeec9d6a2441b531a5621f
Mamun123123123 <almalm90459@gmail.com>
2026-04-15 19:13:20 +0600

    range function in for loop

---
+++ loops.py
@@ -0,0 +1,19 @@
+# a = range(1,21,2)
+# for i in a:
+#     print(i)
+
+# for i in range(10):
+#     print(i)    
+
+# for i in range(16,0,-1):
+#             print(i)
+
+# for i in range(-1,-15,-1):
+#         print(i)            
+
+# for i in range(10,101,10):
+#     print(i)
+
+n = int(input("Enter your number "))
+for i in range(n,(n*10)+1,n):
+    print(i)
